180c5042a0b0c6949115eae0d4670c6c4e23fae0
RodrigoCezarLeao <51330881+RodrigoCezarLeao@users.noreply.github.com>
2020-05-11 11:01:04 -0300

    Add files via upload

--- backup.py
+++ backup.py
@@ -1,48 +1,70 @@
-
-import time
-import sys
+from time import time
+from sys import platform
 from tkinter import Tk
 from tkinter.messagebox import showinfo
-import os
+from os import system
 
         
 
 #Global Variables (path without final \\)
-originPath = "C:\\Users\\Rodrigo"
-destinyPath = "C:\\backupTeste\\"
+originPath = "C:\\Users\\Rodrigo\\Desktop"
+destinyPath = "C:\\backupTeste"
 
-SO = sys.platform
+SO = platform
 imagesExtensions = ["jpg", "jpeg", "png", "gif", "tiff", "bmp", "psd", "raw", "xcf", "svg", "ppm", "pgm", "pbm", "pnm", "cdr"]
 documentsExtensions = ["txt", "pdf", "xps", "doc", "docx", "ppt", "pptx", "xls", "xlsm", "xlsx", "ods", "odf", "odt", "odp", "rtf"]
 songsExtensions = ["mp3", "ogg", "wma", "wav", "aac"]
 videosExtensions = ["avi", "mp4", "wmv", "3gp", "mkv"]
 subtitlesExtensions = ["srt"]
+developsExtensions = ["py", "c", "cpp", "java", "js", "html", "css"]
 
 extensions = {
     "image": imagesExtensions,
     "document": documentsExtensions,
     "song": songsExtensions,
     "video" : videosExtensions,
-    "subtitle": subtitlesExtensions}
+    "subtitle": subtitlesExtensions,
+    "develop": developsExtensions
+}
+
+"""
+mode = 1 -> script python
+mode = 2 -> generate cmd bash
+"""
+mode = 2
 
 
-#Start
-root=Tk()
-root.withdraw()
-showinfo(message="Inicializando o Backup...")
+#Start Python Script
+if mode == 1:
+    root=Tk()
+    root.withdraw()
+    showinfo(message="Inicializando o Backup...")
 
-ini = time.time()
-#Select proper Operating System
-if 'WIN' in SO.upper():   
-    os.system(f"mkdir {destinyPath}")
+    ini = time()
+    #Select proper Operating System
+    if 'WIN' in SO.upper():   
+        system(f"mkdir {destinyPath}")
 
-    for category in extensions:
-        for ext in extensions[category]:     
-            comando = f"xcopy {originPath}\\*.{ext} {destinyPath} /s /c"
-            os.system(comando)
-            
+        for category in extensions:
+            for ext in extensions[category]:     
+                comando = f"xcopy {originPath}\\*.{ext} {destinyPath} /s /c "
+                system(comando)
+                
 
-fim = time.time()            
-showinfo(message=f"Fim do Backup. Tempo de Execução: {round(fim-ini, 2)} s")
-root.destroy()
+    fim = time()            
+    showinfo(message=f"Fim do Backup. Tempo de Execução: {round(fim-ini, 2)} s")
+    root.destroy()
 
+#Start bat generation
+elif mode == 2:
+    if 'WIN' in SO.upper():
+        fp = open("backup.bat", "w")
+        fp.write(f"mkdir {destinyPath}\n")
+        fp.write(f"echo \"Iniciando Backup..\"\n")
+
+        for category in extensions:
+            for ext in extensions[category]:     
+                fp.write(f"xcopy {originPath}\\*.{ext} {destinyPath} /s /c /y\n")
+
+        fp.write(f"echo \"Fim do Backup.\"\ncmd")        
+        fp.close()
